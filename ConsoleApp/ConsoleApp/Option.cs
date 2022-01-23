8d642452ea18dc058bd45d483298c2eb499e2a8c
MDJ0126 <83164393+MDJ0126@users.noreply.github.com>
2022-01-23 22:07:12 +0900

    빌드 패턴 작성

---
+++ ConsoleApp/ConsoleApp/Option.cs
@@ -0,0 +1,39 @@
+﻿public class Option
+{
+    public bool IsPlayBGM { get; private set; } = false;
+    public bool IsEffectBGM { get; private set; } = false;
+    public bool IsPush { get; private set; } = false;
+
+    public class Builder
+    {
+        private Option option = new Option();
+
+        public static Builder Create()
+        {
+            return new Builder();
+        }
+
+        public Builder SetPlayBGM(bool isOn)
+        {
+            option.IsPlayBGM = isOn;
+            return this;
+        }
+
+        public Builder SetEffectBGM(bool isOn)
+        {
+            option.IsEffectBGM = isOn;
+            return this;
+        }
+
+        public Builder SetPush(bool isOn)
+        {
+            option.IsPush = isOn;
+            return this;
+        }
+
+        public Option Build()
+        {
+            return this.option;
+        }
+    }
+}
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,50 +6,20 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Benchmark.Start(LoopTest1);
-            Benchmark.Start(LoopTest2);
-            Benchmark.Start(OutOfRangeException);
-            Benchmark.Start(LoopTest3);
-            Benchmark.Start(LoopTest4);
-            Console.WriteLine(Benchmark.GetRecord());
-        }
+            var option = Option.Builder.Create()
+                                .SetPlayBGM(true)
+                                .SetEffectBGM(true)
+                                .SetPush(true)
+                                .Build();
 
-        static void OutOfRangeException()
-        {
-            int[] a = new int[1];
-            a[2] = 20;
-        }
+            Console.WriteLine($"IsPlayBGM: {option.IsPlayBGM}");
+            Console.WriteLine($"IsEffectBGM: {option.IsEffectBGM}");
+            Console.WriteLine($"IsPush: {option.IsPush}");
 
-        static void LoopTest1()
-        {
-            for (int i = 0; i < 1999; i++)
-            {
-
-            }
-        }
-
-        static void LoopTest2()
-        {
-            for (int i = 0; i < 9999; i++)
-            {
-
-            }
-        }
-
-        static void LoopTest3()
-        {
-            for (int i = 0; i < 999; i++)
-            {
-
-            }
-        }
-
-        static void LoopTest4()
-        {
-            for (int i = 0; i < 9999; i++)
-            {
-
-            }
+            // result:
+            //IsPlayBGM: True
+            //IsEffectBGM: True
+            //IsPush: True
         }
     }
 }
