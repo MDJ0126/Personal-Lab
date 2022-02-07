14e9f994ecd081e079b42d0eacf21e07762d4c09
MDJ0126 <83164393+MDJ0126@users.noreply.github.com>
2022-02-08 00:05:38 +0900

    프로젝트 먹통으로 강제 재생성

--- ConsoleApp/ConsoleApp/PriorityQueue.cs
+++ ConsoleApp/ConsoleApp/PriorityQueue.cs
@@ -28,22 +28,14 @@ public class PriorityQueue<T>
 
     public void MaxHeapInsertSort()
     {
-        //void insert_max_heap(HeapType* h, element item)
-        //{
-        //    int i;
-        //    i = ++(h->heap_size); // 힙 크기를 하나 증가
-
-        //    /* 트리를 거슬러 올라가면서 부모 노드와 비교하는 과정 */
-        //    // i가 루트 노트(index: 1)이 아니고, 삽입할 item의 값이 i의 부모 노드(index: i/2)보다 크면
-        //    while ((i != 1) && (item.key > h->heap[i / 2].key))
-        //    {
-        //        // i번째 노드와 부모 노드를 교환환다.
-        //        h->heap[i] = h->heap[i / 2];
-        //        // 한 레벨 위로 올라단다.
-        //        i /= 2;
-        //    }
-        //    h->heap[i] = item; // 새로운 노드를 삽입
-        //}
+        int i = items.Count;
+        var item = items[i - 1];
+        while (i != 1 && item.Priority > items[(int)(i * 0.5)].Priority)
+        {
+            items[i] = items[(int)(i * 0.5)];
+            i = (int)(i * 0.5);
+        }
+        items[i] = item;
     }
 
     public T Dequeue()
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,34 +1,9 @@
-﻿using System.Threading;
-
-class Program
+﻿class Program
 {
     private static void Main(string[] args)
     {
-        TestClass testClass = new TestClass();
-        for (int i = 0; i < 20; i++)
-        {
-            Thread thread = new Thread(testClass.Calculte);
-            thread.Name = $"Thread ({i})";
-            thread.Start();
-        }
-    }
-
-    private class TestClass
-    {
-        //초기 실행 가능한 쓰레드 2개
-        //최대 실행 가능한 쓰레드 2개
-        Semaphore semaphore = new Semaphore(2, 2);
-
-        public void Calculte()
-        {
-            // 뮤텍스 취득할 때까지 기다린다.
-            semaphore.WaitOne();
-
-            // 1초간 대기 (연산이 처리되는 부분이라 가정)
-            Thread.Sleep(5000);
-
-            // 뮤텍스 해제
-            semaphore.Release();
-        }
+        PriorityQueue<int> priorityQueue = new PriorityQueue<int>();
+        priorityQueue.Enqueue(1, 1);
+        priorityQueue.Enqueue(2, 3);
     }
 }
