94d7cc44832b365c597fa75b34a32cc68e737ade
MDJ0126 <83164393+MDJ0126@users.noreply.github.com>
2022-02-10 00:14:44 +0900

    ref와 out 예제

---
+++ ConsoleApp/ConsoleApp/Example.cs
@@ -0,0 +1,19 @@
+﻿public class Example
+{
+    private class ClassA
+    {
+        public string name = string.Empty;
+        public int count = 0;
+    }
+
+    private void AddCount(ref ClassA classA)
+    {
+        classA.count++;
+    }
+
+    private void Start()
+    {
+        ClassA classA = new ClassA();
+        AddCount(ref classA); // != AddCount(classA);
+    }
+}
