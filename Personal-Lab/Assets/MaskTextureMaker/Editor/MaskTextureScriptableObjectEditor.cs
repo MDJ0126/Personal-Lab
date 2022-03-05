82eb7dc07532d6b4d320af1b941524264c19d040
MDJ0126 <83164393+MDJ0126@users.noreply.github.com>
2022-03-06 01:03:01 +0900

    마스크 작업

---
+++ Personal-Lab/Assets/MaskTextureMaker/Editor/MaskTextureScriptableObjectEditor.cs
@@ -0,0 +1,66 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MaskTextureScriptableObject)), CanEditMultipleObjects]
+public class MaskTextureScriptableObjectEditor : Editor
+{
+    private MaskTextureScriptableObject _maskTextureScriptableObject = null;
+
+    private Texture2D _previewTexture2D = null;
+
+    private void OnEnable()
+    {
+        _maskTextureScriptableObject = (MaskTextureScriptableObject)target;
+        SetPreviewMask();
+    }
+
+    private void SetPreviewMask()
+    {
+        _previewTexture2D = _maskTextureScriptableObject.GetMaskTexture(true);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUI.changed)
+        {
+            SetPreviewMask();
+        }
+    }
+
+    public override bool HasPreviewGUI() => true;
+    public override void OnPreviewGUI(Rect r, GUIStyle background)
+    {
+        var backgroundTex = CreateCheckerTex(new Color(0.1f, 0.1f, 0.1f, 0.5f), new Color(0.2f, 0.2f, 0.2f, 0.5f));
+        GUI.DrawTexture(r, backgroundTex);
+
+        if (_previewTexture2D != null)
+        {
+            var texture = _previewTexture2D;
+            var mask = _maskTextureScriptableObject.maskTexture;
+            GUI.DrawTexture(r, texture, ScaleMode.ScaleToFit);
+        }
+    }
+
+    /// <summary>
+    /// 체크 만들기
+    /// </summary>
+    /// <param name="c0"></param>
+    /// <param name="c1"></param>
+    /// <returns></returns>
+    private Texture2D CreateCheckerTex(Color c0, Color c1)
+    {
+        Texture2D tex = new Texture2D(16, 16);
+        tex.name = "[Generated] Checker Texture";
+        tex.hideFlags = HideFlags.DontSave;
+
+        for (int y = 0; y < 8; ++y) for (int x = 0; x < 8; ++x) tex.SetPixel(x, y, c1);
+        for (int y = 8; y < 16; ++y) for (int x = 0; x < 8; ++x) tex.SetPixel(x, y, c0);
+        for (int y = 0; y < 8; ++y) for (int x = 8; x < 16; ++x) tex.SetPixel(x, y, c0);
+        for (int y = 8; y < 16; ++y) for (int x = 8; x < 16; ++x) tex.SetPixel(x, y, c1);
+
+        tex.Apply();
+        tex.filterMode = FilterMode.Point;
+        return tex;
+    }
+}
---
+++ Personal-Lab/Assets/MaskTextureMaker/Scripts/MaskTextureScriptableObject.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ImageName_MaskTextureData", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class MaskTextureScriptableObject : ScriptableObject
+{
+    private static Dictionary<int, Texture2D> _maskedTextures = new Dictionary<int, Texture2D>();
+
+    public Texture2D texture;
+    public Texture2D maskTexture;
+    public Vector2 ccoordinate;
+
+    /// <summary>
+    /// 마스크 텍스쳐 가져오기
+    /// </summary>
+    /// <param name="isRefresh"></param>
+    /// <returns></returns>
+    public Texture2D GetMaskTexture(bool isRefresh = false)
+    {
+        var instanceId = this.GetInstanceID();
+
+        if (isRefresh)
+            _maskedTextures.Remove(instanceId);
+
+        if (!_maskedTextures.TryGetValue(instanceId, out var texture2D))
+        {
+            var copyTextuer = new Texture2D(texture.width, texture.height, texture.format, texture.mipmapCount, false);
+            Graphics.CopyTexture(texture, copyTextuer);
+
+            var copyMaskTextuer = new Texture2D(maskTexture.width, maskTexture.height, maskTexture.format, maskTexture.mipmapCount, false);
+            Graphics.CopyTexture(maskTexture, copyMaskTextuer);
+
+            texture2D = new Texture2D(maskTexture.width, maskTexture.height, TextureFormat.RGBA32, texture.mipmapCount, false);
+            Graphics.CopyTexture(MakeMaskedTexture(copyTextuer, copyMaskTextuer, ccoordinate), texture2D);
+            texture2D.name = "Masked Texture (Instance)";
+            _maskedTextures.Add(instanceId, texture2D);
+        }
+        return texture2D;
+    }
+
+    /// <summary>
+    /// 대상 텍스쳐를 영역만큼 잘라서 반환
+    /// </summary>
+    /// <param name="texture"></param>
+    /// <param name="area"></param>
+    /// <returns></returns>
+    private Texture2D MakeMaskedTexture(Texture2D texture, Texture2D maskTexture, Vector2 coordinate)
+    {
+        Texture2D result = new Texture2D(Mathf.RoundToInt(maskTexture.width), Mathf.RoundToInt(maskTexture.height), TextureFormat.RGBA32, 1, false);
+
+        var pixels = result.GetPixels();
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            int maskX = i % Mathf.RoundToInt(maskTexture.width);
+            int maskY = i / Mathf.RoundToInt(maskTexture.width);
+            int x = Mathf.RoundToInt(coordinate.x) + maskX;
+            int y = Mathf.RoundToInt(coordinate.y) + maskY;
+            var texturePixel = texture.GetPixel(x, y);
+            var maskPixel = maskTexture.GetPixel(maskX, maskY);
+
+            if (texturePixel.a != 0f)
+            {
+                //texturePixel.r += maskPixel.r;
+                //texturePixel.g += maskPixel.g;
+                //texturePixel.b += maskPixel.b;
+                texturePixel.a = maskPixel.a;
+            }
+
+            result.SetPixel(maskX, maskY, texturePixel);
+        }
+        result.Apply();
+        return result;
+    }
+}
