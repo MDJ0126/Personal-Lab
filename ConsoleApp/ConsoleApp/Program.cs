482e687dfb2139127d0c7afc9ac3f682633af35c
Moon dong jun <83164393+MDJ0126@users.noreply.github.com>
2022-09-20 01:09:06 +0900

    GIF 생성 샘플 코드 및 원문 주소 메모

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,17 +1,88 @@
-﻿using System;
-using System.Threading;
+﻿using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        MyTimer.Start();
-        Thread.Sleep(1000 * 60 * 60 * 12);	// 12시간 딜레이
+        /// Reference : https://debugandrelease.blogspot.com/2018/12/creating-gifs-in-c.html
+        using (FileStream fileStream = new FileStream(@"C:\folder\newfile.gif", FileMode.Create))
+        {
+            using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+            {
+                // header
+                binaryWriter.Write(new char[3] { 'G', 'I', 'F' });
+                binaryWriter.Write(new char[3] { '8', '9', 'a' });
 
-        // OS 타이머의 현재 시간
-        Console.WriteLine(DateTime.Now);
+                // logical screen descriptor
+                binaryWriter.Write((ushort)2);
+                binaryWriter.Write((ushort)2);
+                binaryWriter.Write((byte)0x91);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Write((byte)0);
 
-        // 직접 작성한 프로그램 타이머 현재 시간
-        Console.WriteLine(MyTimer.NowTime);
+                // global color table
+                binaryWriter.Write((byte)255);
+                binaryWriter.Write((byte)255);
+                binaryWriter.Write((byte)255);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Write((byte)255);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Write((byte)255);
+                binaryWriter.Write((byte)0);
+
+                // application extension
+                binaryWriter.Write((byte)0x21);
+                binaryWriter.Write((byte)0xff);
+                binaryWriter.Write((byte)0x0b);
+                binaryWriter.Write('N');
+                binaryWriter.Write('E');
+                binaryWriter.Write('T');
+                binaryWriter.Write('S');
+                binaryWriter.Write('C');
+                binaryWriter.Write('A');
+                binaryWriter.Write('P');
+                binaryWriter.Write('E');
+                binaryWriter.Write('2');
+                binaryWriter.Write('.');
+                binaryWriter.Write('0');
+                binaryWriter.Write((byte)3);
+                binaryWriter.Write((byte)1);
+                binaryWriter.Write((byte)0xff);
+                binaryWriter.Write((byte)0xff);
+                binaryWriter.Write((byte)0);
+
+                // graphic control extension
+                binaryWriter.Write((byte)0x21);
+                binaryWriter.Write((byte)0xf9);
+                binaryWriter.Write((byte)4);
+                binaryWriter.Write((byte)4);
+                binaryWriter.Write((ushort)100);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Write((byte)0);
+
+                // image descriptor
+                binaryWriter.Write((byte)0x2c);
+                binaryWriter.Write((ushort)0);
+                binaryWriter.Write((ushort)0);
+                binaryWriter.Write((ushort)2);
+                binaryWriter.Write((ushort)2);
+                binaryWriter.Write((byte)0);
+
+                // image data
+                binaryWriter.Write((byte)2);
+                binaryWriter.Write((byte)3);
+                binaryWriter.Write((byte)0xc4);
+                binaryWriter.Write((byte)0x14);
+                binaryWriter.Write((byte)5);
+                binaryWriter.Write((byte)0);
+
+                // terminating character
+                binaryWriter.Write((byte)0x3b);
+            }
+        }
     }
 }
